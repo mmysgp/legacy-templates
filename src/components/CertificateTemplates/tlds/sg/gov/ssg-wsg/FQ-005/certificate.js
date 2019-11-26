92ca73bd3ef3c73bd8d20e85e3b555dce54d2f84
Pramod Mishra <rohan.pramod.mishra@accenture.com>
2019-11-26 14:51:10 +0530

    added textAlign property to signature divison

--- src/components/CertificateTemplates/tlds/sg/gov/ssg-wsg/FQ-005/certificate.js
+++ src/components/CertificateTemplates/tlds/sg/gov/ssg-wsg/FQ-005/certificate.js
@@ -23,7 +23,7 @@ export const renderSignature = certificate => (
     </div>
     <div className="col-lg-10 col-12 row d-flex justify-content-center">
       <div className="col-lg-8">
-        <div className="col-lg-4 col-12">
+        <div className="col-lg-4 col-12" style={{ textAlign:"right"}}>
           <img
             style={styles.signatureWidthStyle}
             src={get(
