d9b51e1ced19b8d5e8629b0098527370b9027095
ccewuz <47135828+ccewuz@users.noreply.github.com>
2019-12-04 14:41:03 +0800

    Nus templates new (#59)
    20191204 add new NUS cert store into rendering templates

--- src/components/CertificateTemplates/tlds/sg/edu/nus/common/configure.js
+++ src/components/CertificateTemplates/tlds/sg/edu/nus/common/configure.js
@@ -1,3 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 // NUS store addresses
-export const storeAddresses = ["0xfEbB273495F5C2c4783E23424Fe9773691b57fcB"];
+export const storeAddresses = [
+  "0xfEbB273495F5C2c4783E23424Fe9773691b57fcB", // MetaMask
+  "0xA204F16717fbF580d6A0535b58063fB4fce969fF" // Mainnet
+];
